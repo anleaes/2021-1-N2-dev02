15ff57245fdf07dc4d2dc59fe7ca74b3290ad870
Tailine Wornath <taywornath@gmail.com>
2021-06-19 09:42:05 -0700

    Finalização da implementação do FLIGHTS

--- aeroapp/apps/flights/models.py
+++ aeroapp/apps/flights/models.py
@@ -3,6 +3,8 @@ from aircrafts.models import Aircraft
 
 # Create your models here.
 class Flight(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     quantity = models.IntegerField('Quantidade de Tickets',null=True, blank=True)
     is_full = models.BooleanField('Voo cheio', default=False)
     aircraft = models.ForeignKey(Aircraft, on_delete=models.CASCADE, blank=True)
@@ -15,14 +17,15 @@ class Flight(models.Model):
     def __str__(self):
         return self.name
 
-
-class Aircraft(models.Model):
-    aircraft = models.ForeignKey(Aircraft, on_delete=models.CASCADE)
-
-    class Meta:
-        verbose_name = 'Aeronave do voo'
-        verbose_name_plural = 'Aeronaves do voo'
-        ordering =['id']
-
-    def __str__(self):
-        return self.aircraft.name
+#
+#class Aircraft(models.Model):
+#    aircraft = models.ForeignKey(Aircraft, on_delete=models.CASCADE)
+#
+#    class Meta:
+#        verbose_name = 'Aeronave do voo'
+#        verbose_name_plural = 'Aeronaves do voo'
+#        ordering =['id']
+#
+#    def __str__(self):
+#       return self.aircraft.name
+#
