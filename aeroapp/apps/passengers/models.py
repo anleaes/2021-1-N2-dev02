cee965445738e9b18a461d896cb9454f9c1e3b2a
Tailine Wornath <taywornath@gmail.com>
2021-06-24 05:47:24 -0700

    partial implementation of TICKETS

--- aeroapp/apps/passengers/models.py
+++ aeroapp/apps/passengers/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db import models
 from socialnetworks.models import Socialnetwork
 
 # Create your models here.
---
+++ aeroapp/apps/tickets/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from flights.models import Flight
+from passengers.models import Passenger
+
+# Create your models here.
+class Ticket(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    total_price = models.FloatField('Preco Total', null=True, blank=True, default=0.0)
+    STATUS_CHOICES = (
+        ('Em andamento', 'Em andamento'),
+        ('Finalizado', 'Finalizado'),
+        ('Cancelado', 'Cancelado'),
+    )
+    status = models.CharField('Status', max_length=20, choices=STATUS_CHOICES, null=True, blank=True, default='Em andamento')
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
+    ticket_passenger = models.ManyToManyField(Passenger, through='TicketPassenger', blank=True)
+    
+    class Meta:
+        verbose_name = 'Ticket'
+        verbose_name_plural = 'Tickets'
+        ordering =['-created_on']
+
+    def __str__(self):
+        return "%s" % (self.total_price) 
+
+
+class TicketPassenger(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    seat = models.CharField('Assento',max_length=4,null=True, blank=True,default=0)
+    unitary_price = models.FloatField('Preco unitario',null=True, blank=True, default=0.0)
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    passenger = models.ForeignKey(Passenger, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Passageiro do ticket'
+        verbose_name_plural = 'Passageiros do ticket'
+        ordering =['id']
+
+    def __str__(self):
+        return "%s" % (self.passenger) 
+
