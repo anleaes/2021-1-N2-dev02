5225d1344159af59922b63b169575e2fd3204ddd
Tailine Wornath <taywornath@gmail.com>
2021-06-24 08:05:21 -0700

    error to be solved

--- aeroapp/apps/tickets/urls.py
+++ aeroapp/apps/tickets/urls.py
@@ -5,7 +5,7 @@ app_name = 'tickets'
 
 urlpatterns = [
     path('', views.list_tickets, name='list_tickets'),
-    path('adicionar/<int:id_ticket>/', views.add_ticket, name='add_ticket'),
+    path('adicionar/<int:id_flight>/', views.add_ticket, name='add_ticket'),
     path('excluir/<int:id_ticket>/', views.delete_ticket, name='delete_ticket'),
     path('adicionar-passageiro/<int:id_ticket_passenger>/', views.add_ticket_passenger, name='add_ticket_passenger'),
     path('excluir-passageiro/<int:id_ticket_passenger>/', views.delete_ticket_passenger, name='delete_ticket_passenger'),
