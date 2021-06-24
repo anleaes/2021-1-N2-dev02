309e264ff04997cbb1348d58a89ed8c2a9359924
Luiza <luizav24@gmail.com>
2021-06-24 16:26:52 -0300

    ajustes lgun

--- aeroapp/aeroapp/settings.py
+++ aeroapp/aeroapp/settings.py
@@ -13,6 +13,8 @@ import os
 import sys 
 from pathlib import Path
 from decouple import config
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #ANTIGO: BASE_DIR = Path(__file__).resolve().parent.parent
